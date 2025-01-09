8ecab627b871b769aabd74812a62d9fa2b538f53
hx-the-great <fanrongli1507@gmail.com>
2025-01-09 14:22:30 +0800

    fixed ui

--- scripts/phoenixAI.js
+++ scripts/phoenixAI.js
@@ -5,7 +5,11 @@ const sendButton1 = document.querySelector('.js-send');
 sendButton1.addEventListener('touch', send);
 
 const inputBar = document.querySelector('.js-input');
-inputBar.addEventListener('keydown', send);
+inputBar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    send();
+  }
+});
 
 function send() {
   const input = document.querySelector('.js-input').value;
@@ -16,7 +20,7 @@ function send() {
   const messageHTML = `<div class="message">${input}</div>`;
   const messageContainer = document.querySelector('.js-text');
 
-  messageContainer.innerHTML += messageHTML;
+  messageContainer.innerHTML = messageHTML + messageContainer.innerHTML;
 
   document.querySelector('.js-input').value = '';
 }
